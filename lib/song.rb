f711cbfb89f0eb33ab75dd68d4fe799b4e4cdf08
David Jaworski <jaws360@gmail.com>
2019-11-01 16:13:27 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+  def title
+    @title 
+  end
+
+  def title=(name)
+    @title = name
+  end
+
+
+  def artist
+    @artist
+  end
+
+  def artist=(name)
+    @artist = name
+  end
+end
