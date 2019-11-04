a97852588411e7f29fde4657f5f3c902726e7ea8
github username <github email address>
2019-11-04 07:51:26 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,26 @@
+
+
+
+class Post
+
+    def initialize
+        @title
+    end
+
+    def title
+        return @title
+    end
+
+    def title=(value)
+        @title = value
+    end
+
+    def author
+        return @author
+    end
+
+    def author=(value)
+        @author = value
+    end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+
+    def initialized
+        @title
+    end
+
+    def title
+        return @title
+    end
+
+    def title=(value)
+        @title = value
+    end
+
+    def artist
+        return @artist
+    end
+
+    def artist=(value)
+        @artist = value
+    end
+    
+
+end
